623a09833c6e0ce1f0c8263e20d057ba144f385b
Sindre Sivertsen <sindrejohan1@gmail.com>
2021-04-30 12:37:06 +0200

    Add phi function. Remove pychache

--- numberTheory.py
+++ numberTheory.py
@@ -1,4 +1,5 @@
 import math
+from typing import List
 
 def get_group_generators(p) -> List[int]:
     """
@@ -7,6 +8,7 @@ def get_group_generators(p) -> List[int]:
     """
     return  list(filter(lambda x: math.gcd(x, p) == 1, range(p)))
 
+def phi(n: int) -> int: return len(get_group_generators(n))
 
 def get_inverse_modulo(a, n) -> int:
     return pow(a, -1, n)
